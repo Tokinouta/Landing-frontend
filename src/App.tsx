9bdceac9ed426c9e1a2ea1608ff254d360a385cb
Bob User <shangrila-1100@hotmail.com>
2021-04-15 16:40:47 +0800

    Finally an available version of chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { createRef, useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './Components/Header';
 import { BrowserRouter } from 'react-router-dom';
 import Chart from 'chart.js/auto';
 import { Curves } from './Components/Curves';
+import { ChartCom } from './Components/ChartComponent';
+import { Props } from './Components/types';
 // import { Curves } from './Components/Curves';
 
 function App() {
-  const data = {
+  let dataIni = {
     labels: ['1', '2', '3', '4', '5', '6'],
     datasets: [
       {
@@ -21,7 +23,7 @@ function App() {
     ],
   };
 
-  const options = {
+  let optionsIni = {
     scales: {
       yAxes: [
         {
@@ -32,7 +34,9 @@ function App() {
       ],
     },
   };
-
+  // let rr: Props = null;
+  let [data, setData] = useState(dataIni);
+  let [option, setOption] = useState(optionsIni);
   useEffect(() => {
     new Chart(document.getElementById('chart'), {
       type: 'bar',
@@ -70,7 +74,53 @@ function App() {
         },
       },
     });
-  });
+  }, []);
+  let ref = createRef<typeof Curves>();
+  const [ra, setRa] = useState<number>(0);
+
+  let r: Props = {
+    type: 'line',
+    data: {
+      labels: ['1', '2', '3', '4', '5', '6'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          fill: false,
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgba(255, 99, 132, 0.2)',
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+  const Clicked = () => {
+    // let t = {
+    //   ...data.current,
+    //   datasets: [],
+    //   labels: [],
+    // };
+    // console.log(Object.is(t, data.current));
+    // data.current.datasets[0].data.push(0);
+    // data.current.labels.push('0');
+    setData((prev) => {
+      prev.datasets[0].data.push(0);
+      prev.labels.push('0');
+      return prev;
+    });
+    setRa((prev) => prev + 1);
+    console.log(r);
+    r.data.datasets[0].data.push(0);
+    r.data.labels.push('0');
+    // console.log(ref.current.current);
+    // 如何触发重绘
+  };
   return (
     <BrowserRouter>
       <div className="App">
@@ -86,14 +136,109 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React {ra}
           </a>
         </header>
         <canvas id="chart"></canvas>
-        <Curves data={data} options={options} type="line"></Curves>
+        <button onClick={Clicked}>rarara</button>
+        <ChartCom data={r.data} type={r.type} options={r.options}></ChartCom>
+        {/* <Curves data={data} options={option} type="line" ref={ref}></Curves> */}
       </div>
     </BrowserRouter>
   );
+}
+
+interface MyState {
+  ra: number;
+  chartConfig: Props;
+}
+export class Ass extends React.Component<any, MyState> {
+  ref: React.RefObject<ChartCom>;
+  // r: Props;
+  constructor(props: any) {
+    super(props);
+    // this.r = ;
+    this.state = {
+      ra: 0,
+      chartConfig: {
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: [
+            {
+              label: '# of Votes',
+              data: [],
+              fill: false,
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgba(255, 99, 132, 0.2)',
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      },
+    };
+    this.ref = React.createRef<ChartCom>();
+  }
+
+  componentDidMount() {
+    console.log('this.componentDidMount');
+  }
+
+  Clicked = () => {
+    console.log('clicked triggered', this.state.chartConfig);
+
+    this.setState(null, () => {
+      console.log(this.state.chartConfig);
+      this.state.chartConfig.data.datasets[0].data.push(1);
+      this.state.chartConfig.data.labels.push('0');
+      this.ref.current?.updateChart();
+    });
+    this.setState((prev, prop) => ({
+      ra: prev.ra + 1,
+    })); // this.state.ra = 0;
+
+    // console.log(ref.current.current);
+    // 如何触发重绘
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Header></Header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React {this.state.ra}
+            </a>
+          </header>
+          <canvas id="chart"></canvas>
+          <button onClick={this.Clicked}>rarara</button>
+          <ChartCom
+            data={this.state.chartConfig.data}
+            type={this.state.chartConfig.type}
+            options={this.state.chartConfig.options}
+            ref={this.ref}
+          ></ChartCom>
+          {/* <Curves data={data} options={option} type="line" ref={ref}></Curves> */}
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/Components/ChartComponent.tsx
+++ src/Components/ChartComponent.tsx
@@ -6,6 +6,7 @@ import React, {
   useMemo,
   useCallback,
   forwardRef,
+  useReducer,
 } from 'react';
 // eslint-disable-next-line no-unused-vars
 import { Props } from './types';
@@ -14,6 +15,8 @@ import Chart from 'chart.js/auto';
 import merge from 'lodash/merge';
 import assign from 'lodash/assign';
 import find from 'lodash/find';
+// import { Chart, Chart } from 'chart.js';
+// import { Chart } from 'chart.js';
 
 const ChartComponent = forwardRef<Chart | undefined, Props>((props, ref) => {
   const {
@@ -29,135 +32,210 @@ const ChartComponent = forwardRef<Chart | undefined, Props>((props, ref) => {
 
   const canvas = useRef<HTMLCanvasElement | null>(null);
 
-  const computedData = useMemo<Chart.ChartData>(() => {
-    if (typeof data === 'function') {
-      return canvas.current ? data(canvas.current) : {};
-    } else return data;
-  }, [data]);
-
-  //   const [chart, setChart] = useState<Chart>(null);
-  let chart = useRef<Chart | null>(null);
-  useImperativeHandle<Chart | undefined, Chart | undefined>(
-    ref,
-    () => chart.current,
-    [chart],
+  const computedData = useMemo<Chart.ChartData>(() => data, [data]);
+  //   const computedData = Object.assign({}, data);
+  console.log(Object.is(computedData, data));
+  let [chart, setChart] = useState<Chart>();
+  //   const charti :Chart = null;
+  const [charti, dispacher] = useReducer(
+    (state: Chart, action: { type: string }) => {
+      switch (action.type) {
+        case 'create':
+          console.log('before create', state);
+          if (!state) {
+            state = new Chart(canvas.current, {
+              type,
+              data: computedData,
+              options,
+              plugins,
+            });
+          }
+          console.log('after create', state);
+          return state;
+        case 'update':
+          console.log('before update', state);
+          if (!state) return;
+          else {
+            state.update();
+            console.log('chart updated');
+            return { ...state };
+            // return state;
+          }
+        case 'destroy':
+          console.log('started destroying');
+          console.log(state);
+          if (state) state.destroy();
+          return;
+        default:
+          throw new Error();
+      }
+      //   return state.update();
+    },
+    null,
   );
+  // new Chart(canvas.current, {
+  //   type,
+  //   data: computedData,
+  //   options,
+  //   plugins,
+  // }),
+  //   let chart = useRef<Chart | null>(null);
+  let [ra, setRa] = useState<number>(0);
+  useImperativeHandle<Chart | undefined, Chart | undefined>(ref, () => chart, [
+    chart,
+  ]);
 
   const renderChart = useCallback(() => {
     if (!canvas.current) return;
 
-    chart.current = new Chart(canvas.current, {
-      type,
-      data: computedData,
-      options,
-      plugins,
+    setChart((prev: Chart | null | undefined) => {
+      console.log(prev);
+      if (prev) return;
+      prev = new Chart(canvas.current, {
+        type,
+        data: computedData,
+        options,
+        plugins,
+      });
+      return prev;
     });
+    console.log(chart);
     // setChart(temp);
     // console.log(Object.is(temp, chart));
     console.log('chart set');
-  }, [computedData, options, plugins, type]);
+  }, [chart, computedData, options, plugins, type]);
 
   const onClick = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!chart.current) return;
+    if (!chart) return;
 
     const { getDatasetAtEvent, getElementAtEvent, getElementsAtEvent } = props;
 
-    getDatasetAtEvent &&
-      getDatasetAtEvent(chart.current.getDatasetAtEvent(e), e);
-    getElementAtEvent &&
-      getElementAtEvent(chart.current.getElementAtEvent(e), e);
-    getElementsAtEvent &&
-      getElementsAtEvent(chart.current.getElementsAtEvent(e), e);
+    getDatasetAtEvent && getDatasetAtEvent(chart.getDatasetAtEvent(e), e);
+    getElementAtEvent && getElementAtEvent(chart.getElementAtEvent(e), e);
+    getElementsAtEvent && getElementsAtEvent(chart.getElementsAtEvent(e), e);
   };
 
   const updateChart = useCallback(() => {
-    if (!chart) return;
+    setChart((prev: Chart) => {
+      if (!chart) return;
 
-    // if (options) {
-    //   chart.current.options = merge(chart.current.options, options);
-    // }
+      if (options) {
+        chart.options = options;
+      }
 
-    if (!chart.current.config.data) {
-      chart.current.config.data = computedData;
-      chart.current.update();
-      return;
-    }
+      if (!chart.config.data) {
+        chart.config.data = computedData;
+        chart.update();
+        return;
+      }
 
-    // const { datasets: newDataSets = [], ...newChartData } = computedData;
-    // const { datasets: currentDataSets = [] } = chart.current.config.data;
+      chart.config.dataset = computedData;
+      // const { datasets: newDataSets = [], ...newChartData } = computedData;
+      // const { datasets: currentDataSets = [] } = chart.config.data;
 
-    // copy values
-    // assign(chart.current.config.data, newChartData);
-    // chart.current.config.datasets
-    // chart.current.config.data.datasets = newDataSets.map(
-    //   (newDataSet: { label: any; type: any; data: string | any[] }) => {
-    //     // given the new set, find it's current match
-    //     console.log(newDataSet);
-    //     const currentDataSet = find(
-    //       currentDataSets,
-    //       (d: { label: any; type: any }) =>
-    //         d.label === newDataSet.label && d.type === newDataSet.type,
-    //     );
+      // copy values
+      // assign(chart.config.data, newChartData);
+      // chart.config.datasets
+      // chart.config.data.datasets = newDataSets.map(
+      //   (newDataSet: { label: any; type: any; data: string | any[] }) => {
+      //     // given the new set, find it's current match
+      //     console.log(newDataSet);
+      //     const currentDataSet = find(
+      //       currentDataSets,
+      //       (d: { label: any; type: any }) =>
+      //         d.label === newDataSet.label && d.type === newDataSet.type,
+      //     );
 
-    //     // There is no original to update, so simply add new one
-    //     if (!currentDataSet || !newDataSet.data) return newDataSet;
+      //     // There is no original to update, so simply add new one
+      //     if (!currentDataSet || !newDataSet.data) return newDataSet;
 
-    //     console.log(currentDataSet);
-    //     if (!currentDataSet.data) {
-    //       currentDataSet.data = [];
-    //     }
-    //     // } else {
-    //     //   console.log(newDataSet.data.length);
-    //     //   currentDataSet.data.splice(newDataSet.data.length - 1);
-    //     // }
+      //     console.log(currentDataSet);
+      //     if (!currentDataSet.data) {
+      //       currentDataSet.data = [];
+      //     }
+      //     // } else {
+      //     //   console.log(newDataSet.data.length);
+      //     //   currentDataSet.data.splice(newDataSet.data.length - 1);
+      //     // }
 
-    //     // copy in values
-    //     assign(currentDataSet.data, newDataSet.data);
+      //     // copy in values
+      //     assign(currentDataSet.data, newDataSet.data);
 
-    //     // apply dataset changes, but keep copied data
-    //     const obj = {
-    //       ...currentDataSet,
-    //       ...newDataSet,
-    //       data: currentDataSet.data,
-    //     };
-    //     console.log(obj);
-    //     return obj;
-    //   },
-    // );
-    console.log(chart.current.config.data.datasets);
-    console.log(chart.current.data);
-    chart.current.update();
-    console.log('chart updated');
+      //     // apply dataset changes, but keep copied data
+      //     const obj = {
+      //       ...currentDataSet,
+      //       ...newDataSet,
+      //       data: currentDataSet.data,
+      //     };
+      //     console.log(obj);
+      //     return obj;
+      //   },
+      // );
+      console.log(chart.config.data.datasets);
+      console.log(chart.data);
+      chart.update();
+      console.log('chart updated');
+    });
   }, [chart, computedData, options]);
 
   const destroyChart = useCallback(() => {
     console.log('destroy called');
     console.log(chart);
-    if (chart) {
+    // if (chart) {
+    //   }
+    setChart((prev: Chart) => {
       console.log('started destroying');
-      chart.current.destroy();
-    }
+      console.log(prev);
+      if (prev) prev.destroy();
+      return prev;
+    });
   }, [chart]);
 
   useEffect(() => {
     console.log('first useEffect');
-    renderChart();
-    console.log(chart);
+    // renderChart();
+    dispacher({ type: 'create' });
+    console.log(charti);
     return () => {
-      destroyChart();
+      //   destroyChart();
+      dispacher({ type: 'destroy' });
       console.log('destroyed');
     };
-  }, [chart, destroyChart, renderChart]);
+  }, []);
 
   useEffect(() => {
     console.log('second useEffect');
     if (redraw) {
-      destroyChart();
-      renderChart();
+      //   destroyChart();
+      //   renderChart();
+      dispacher({ type: 'destroy' });
+      dispacher({ type: 'create' });
       console.log('redrew');
     } else {
-      updateChart();
-      console.log('updated');
+      //   updateChart();
+      console.log('ready to dispatch');
+      dispacher({ type: 'update' });
+      console.log('ra');
+      //   setRa(1);
+      //   if (!chart) return;
+
+      //   if (options) {
+      //     chart.options = options;
+      //   }
+
+      //   if (!chart.config.data) {
+      //     chart.config.data = computedData;
+      //     chart.update();
+      //     return;
+      //   }
+
+      //   chart.config.dataset = computedData;
+
+      //   console.log(chart.config.data.datasets);
+      //   console.log(chart.data);
+      //   chart.update();
+      //   console.log('chart updated');
+      //   console.log('updated');
     }
   });
 
@@ -172,5 +250,56 @@ const ChartComponent = forwardRef<Chart | undefined, Props>((props, ref) => {
     />
   );
 });
+
+export class ChartCom extends React.Component<Props, any, any> {
+  chartCanvas: React.RefObject<HTMLCanvasElement>;
+  chart: Chart;
+  config: Props | null;
+  constructor(props: Props) {
+    super(props);
+    this.chartCanvas = React.createRef();
+    this.chart = null;
+    this.config = null;
+  }
+
+  componentDidMount() {
+    //   this.props.
+    let configc = { ...this.props };
+
+    if (!this.chart) {
+      this.chart = new Chart(this.chartCanvas.current, configc);
+    }
+    console.log(this.chart);
+    console.log('finished initializing');
+  }
+
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.destroy();
+      console.log('destroyed', this.chart);
+      console.log('destroyed');
+    }
+  }
+  updateChart() {
+    if (this.chart) {
+      console.log('start to update chart');
+      //   console.log(this.chart, prev);
+      //   let configc = { ...this.props };
+      //   this.chart.config = prev;
+      this.chart.update('show');
+      //   this.chart.update('show');
+      //   this.chart.update('show');
+      console.log('finished updating chart');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas ref={this.chartCanvas}></canvas>
+      </div>
+    );
+  }
+}
 
 export default ChartComponent;
